a5e94f211c6f106e872d5c69aa368b51c6aaf6a4
Tsung Hung <hungmasterj@gmail.com>
2015-05-24 20:30:00 -0700

    brought in waypoints.txt and utilized promise systems

--- payload.js
+++ payload.js
@@ -1,14 +1,28 @@
 var fs = require('fs');
 var path = require('path');
-var _ = require('lodash');
 
 var filePath = path.join(__dirname, "data/raw/waypoints.txt")
 
-var output = fs.readFileSync(filePath, 'utf-8', function(err, data) {
-  if (err) {
-    throw err;
-  }
-  return data;
-});
+// var output = JSON.parse(fs.readFileSync(filePath, 'utf-8'));//, function(err, data) {
+//   // if (err) {
+//     // throw err;
+//   // }
+//   // output = JSON.parse(data);
+// // });
+//
+// module.exports = output;
+//
+//
+//
 
-module.exports = output;
+module.exports = function () {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, "utf-8", function (err, output) {
+      if (err) {
+        reject(err);
+        // throw err;
+      }
+      resolve(JSON.parse(output));
+    });
+  });
+};
